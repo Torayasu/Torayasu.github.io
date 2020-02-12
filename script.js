77d84546bf704b7ff0eed71b3e9ba7e824e19834
Jakub Wilk <jakub.wilk@pl.abb.com>
2020-02-12 06:08:49 +0100

    Module 23.1, deployment ready

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = ' https://evening-tundra-69079.herokuapp.com/v1/task/';
-  const trelloApiRoot = ' https://evening-tundra-69079.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://evening-tundra-69079.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://evening-tundra-69079.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
